e4c418e1eeaef73a92013d0075e2a2153729a1dd
Taisekwa Chikazhe <taisekwa.chikazhe@dairynz.co.nz>
2020-10-20 11:13:30 +1300

    second time commit

--- Test github flow.R
+++ Test github flow.R
@@ -2,3 +2,6 @@
 install.packages("usethis")
 library(usethis)
 use_git_config(user.name = "Taisekwa Chikazhe", user.email = "taisekwa.chikazhe@dairynz.co.nz")
+usethis::browse_github_pat()
+usethis::edit_r_environ()
+GITHUB_PAT=2fa1c52b6dbde029f1a396bca6bb135b8d19ceb9
