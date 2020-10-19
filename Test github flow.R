6d0b5bdbe3d569dbdf58ca3191dbebdd8d0d334d
Taisekwa Chikazhe <taisekwa.chikazhe@dairynz.co.nz>
2020-10-20 10:54:52 +1300

    learning how to commit

--- Test github flow.R
+++ Test github flow.R
@@ -1,1 +1,4 @@
-# Test for git flow
+# Test script to test git flow
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "Taisekwa Chikazhe", user.email = "taisekwa.chikazhe@dairynz.co.nz")
